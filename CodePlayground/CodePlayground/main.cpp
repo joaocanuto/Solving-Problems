00632043295b105c59ef28e167b10ec9dc8deb02
João Guilherme <joaogacanuto@gmail.com>
2023-11-30 14:02:12 -0300

    coding using xcode

---
+++ CodePlayground/CodePlayground/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CodePlayground
+//
+//  Created by Joao Guilherme Araujo Canuto on 30/11/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
